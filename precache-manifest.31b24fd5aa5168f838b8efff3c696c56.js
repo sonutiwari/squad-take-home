aee5d12be4ae1a5cca47a21a650c2b7b71ac0fa3
sonutiwari <ankorha@gmail.com>
2020-09-07 12:49:06 +0530

    Updates

---
+++ precache-manifest.31b24fd5aa5168f838b8efff3c696c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f4bf13d54530cdd73d8",
+    "url": "/squad-take-home/static/css/main.5ae69a39.chunk.css"
+  },
+  {
+    "revision": "2f4bf13d54530cdd73d8",
+    "url": "/squad-take-home/static/js/main.1bdcb6f7.chunk.js"
+  },
+  {
+    "revision": "7a7166c45101fd2c03c7",
+    "url": "/squad-take-home/static/js/runtime~main.bf3e0d1e.js"
+  },
+  {
+    "revision": "08b48b2c404229ae57ac",
+    "url": "/squad-take-home/static/css/2.fc5a2e34.chunk.css"
+  },
+  {
+    "revision": "08b48b2c404229ae57ac",
+    "url": "/squad-take-home/static/js/2.90c2b1dd.chunk.js"
+  },
+  {
+    "revision": "151470f627192ea6547541f1155f2a0a",
+    "url": "/squad-take-home/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/squad-take-home/precache-manifest.e2e73526f693d7d8a5ffd274cfb8fff7.js"
+  "/squad-take-home/precache-manifest.31b24fd5aa5168f838b8efff3c696c56.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/squad-take-home/index.html"), {
+workbox.routing.registerNavigationRoute("/squad-take-home/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
